ebae44a216cb4e29babc6e966685bcab35d63a72
vwiltz <vanessa.wiltshire@mail.utoronto.ca>
2024-02-12 13:33:13 -0500

    new changes 	modified:   merged_gdf_DAs.geojson 	modified:   script.js

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-mapboxgl.accessToken = '...'; //Add default public map token from your Mapbox account
+mapboxgl.accessToken = 'pk.eyJ1IjoidndpbHR6IiwiYSI6ImNscmZ0N3liOTA1Mmkybm8xeGU0cmZuOW8ifQ.EpQc24rhxsadjwWf3mvoiQ'; //Add default public map token from your Mapbox account
 const map = new mapboxgl.Map({
-    container: '...', // map container ID
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [..., ...], // starting position [lng, lat]
-    zoom: ..., // starting zoom
+    container: 'my-map', // map container ID
+    style: 'mapbox://styles/mapbox/light-v11', // style URL
+    center: [43.65314167329393, -79.38385683409712], // starting position [lng, lat]
+    zoom: 10 // starting zoom
 });
 
-
-
-map.addSource('buildings-data', {
-    type: 'geojson',
-    data: 'merged_gdf_DAs.geojson' // Your URL to your buildings.geojson file
+map.on('load', () => {
+    // Add a data source from a GeoJSON file
+    map.addSource('DV-data', {
+        type: 'geojson',
+        data: 'https://github.com/vwiltz/ggr472-lab2/blob/58878bf60cbd3aaa288a855725db4889186f1488/merged_gdf_DAs.geojson' // Your URL to your buildings.geojson file
+    });
+    map.addLayer({
+        'id': 'buildings-point',
+        'type': 'circle',
+        'source': 'DV-data',
+        'paint': {
+            'circle-radius': 5,
+            'circle-color': '#007cbf'
+        }
+    });
 });
-map.addLayer({
-    'id': 'buildings-point',
-    'type': 'circle',
-    'source': 'buildings-data',
-    'paint': {
-        'circle-radius': 5,
-        'circle-color': '#007cbf'
