fab0b885de207b5c6e5c74c38a973637cf210655
vwiltz <vanessa.wiltshire@mail.utoronto.ca>
2024-02-13 14:19:08 -0500

    comments

--- script.js
+++ script.js
@@ -14,36 +14,41 @@ map.on('load', () => {
     map.addSource('neighbourhoods', {
         type: 'geojson',
         data: 'https://raw.githubusercontent.com/vwiltz/ggr472-lab2/main/Neighbourhoods.geojson'
-
     });
-
+    // TCHC housing locations
     map.addSource('TCHC-locations', {
         'type': 'vector',
         'url': 'mapbox://vwiltz.3wff5s3i'
     });
-
+    // polygon fill (neighbourhoods)
     map.addLayer({
-        'id': 'neighbs-fill',
+        'id': 'neighbs-fill', //  unique layer ID
         'type': 'fill',
-        'source': 'neighbourhoods',
+        'source': 'neighbourhoods',  // data source: neighbourhood boundaries
         'paint': {
             'fill-color': '#ffffff',
             'fill-opacity': 1
-
+        }
+    });
+    // polygon outlines
+    map.addLayer({
+        'id': 'neighbs-outline', // unique layer ID
+        'type': 'line',
+        'source': 'neighbourhoods', // data source: neighbourhood boundaries
+        'paint': {
+            'line-color': 'black',
+            'line-width': 1
         }
     });
 
     map.addLayer({
-        'id': 'neighbs-outline',
-        'type': 'line',
-        'source': 'neighbourhoods',
+        'id': 'locations', // unique layer ID
+        'type': 'circle',
+        'source': 'TCHC-locations', // data source: TCHC Community Housing data
         'paint': {
-            'line-color': 'black',
-            'line-width': 1
-
-
-        }
+            'circle-radius': 2,
+            'circle-color': '#000000'
+        },
+        'source-layer': 'Community_Housing_Data-btz8qw'
     });
-
-
 });
