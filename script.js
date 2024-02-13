88f6876a06fce31265053807275c661d0d2480ec
vwiltz <vanessa.wiltshire@mail.utoronto.ca>
2024-02-13 12:44:55 -0500

    more changes

--- script.js
+++ script.js
@@ -4,20 +4,24 @@ mapboxgl.accessToken = 'pk.eyJ1IjoidndpbHR6IiwiYSI6ImNscmZ0N3liOTA1Mmkybm8xeGU0c
 const map = new mapboxgl.Map({
     container: 'my-map',
     style: 'mapbox://styles/mapbox/streets-v12',
-    center: [-79.365,43.725],
+    center: [-79.365, 43.725],
     zoom: 11,
 });
 
-// map.on('load', () => {
-
-// add data source containing geoJSON data
-
-// }
-
+// add a data source from geojson
+map.on('load', () => {
+    map.addSource('TCHC_data', {
+        type: 'geojson',
+        data: 'https://raw.githubusercontent.com/vwiltz/ggr472-lab2/main/Community%20Housing%20Data.geojson'
+    }
+    });
 
 map.addLayer({
-
-
-
-    
-})
+    'id': 'location-points',
+    'type': 'circle',
+    'source': 'TCHC_data',
+    'paint': {
+        'circle-radius': 5,
+        'circle-color': '#000000'
+    }
+});
